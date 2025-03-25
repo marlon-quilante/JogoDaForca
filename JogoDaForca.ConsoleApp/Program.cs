d71f6b8f5a82fb51a6ed61a4c4546d383541a83b
Marlon Quilante <marlonquilante@gmail.com>
2025-03-25 20:37:53 -0300

    Implementada a lógica de acerto ou enforcamento

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -6,7 +6,7 @@
         {
             while (true)
             {
-                string palavraSecreta = "MELANCIA";
+                string palavraSecreta = "UVA";
 
                 char[] letrasEncontradas = new char[palavraSecreta.Length];
 
@@ -15,18 +15,65 @@
                     letrasEncontradas[caractere] = '_';
                 }
 
-                string dicaDaPalavra = String.Join(" ", letrasEncontradas);
+                int quantidadeErros = 0;
 
-                Console.Clear();
-                Console.WriteLine("----------------------------");
-                Console.WriteLine("Jogo da Forca");
-                Console.WriteLine("----------------------------");
-                Console.WriteLine("Palavra secreta: " + dicaDaPalavra);
-                Console.WriteLine("----------------------------");
+                bool jogadorEnforcou = false;
+                bool jogadorAcertou = false;
 
-                Console.Write("Digite uma letra: ");
-                char chute = Console.ReadLine()[0]; //obtém apenas um caracter do que o usuário digita
-                Console.WriteLine(chute);
+                do
+                {
+                    string dicaDaPalavra = String.Join(" ", letrasEncontradas);
+
+                    Console.Clear();
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("Jogo da Forca");
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("Palavra secreta: " + dicaDaPalavra);
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("Erros: " + quantidadeErros);
+                    Console.WriteLine("----------------------------");
+
+                    Console.Write("Digite uma letra: ");
+                    char chute = Console.ReadLine()[0]; //obtém apenas um caracter do que o usuário digita
+                    Console.WriteLine(chute);
+
+                    bool letraFoiEncontrada = false;
+
+                    for (int contador = 0; contador < palavraSecreta.Length; contador++)
+                    {
+                        char letraAtual = palavraSecreta[contador];
+
+                        if (chute == letraAtual)
+                        {
+                            letrasEncontradas[contador] = letraAtual;
+                            letraFoiEncontrada = true;
+                        }
+                    }
+
+                    if (letraFoiEncontrada == false)
+                        quantidadeErros++;
+
+                    dicaDaPalavra = String.Join("", letrasEncontradas);
+
+                    jogadorAcertou = dicaDaPalavra == palavraSecreta;
+                    jogadorEnforcou = quantidadeErros > 5;
+
+                    if (jogadorAcertou)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("----------------------------");
+                        Console.WriteLine("Você acertou a palavra secreta! Era: " + palavraSecreta);
+                        Console.WriteLine("----------------------------");
+                    } else if (jogadorEnforcou)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("----------------------------");
+                        Console.WriteLine("Que azar, tente novamente! A palavra era: " + palavraSecreta);
+                        Console.WriteLine("----------------------------");
+                    }
+
+                } while (jogadorAcertou == false && jogadorEnforcou == false);
+
                 Console.ReadLine();
             }
         }
